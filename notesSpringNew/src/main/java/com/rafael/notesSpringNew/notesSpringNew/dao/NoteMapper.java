fa93fa2bb615cc090d14e29833436d9c8d1c2427
Rafail Abdyushev <rafaelab@mail.ru>
2023-02-26 15:54:38 +0300

    data improved

--- notesSpringNew/src/main/java/com/rafael/notesSpringNew/notesSpringNew/dao/NoteMapper.java
+++ notesSpringNew/src/main/java/com/rafael/notesSpringNew/notesSpringNew/dao/NoteMapper.java
@@ -22,12 +22,11 @@ public class NoteMapper implements RowMapper<Note> {
 
     @Override
     public Note mapRow(ResultSet rs, int rowNum) throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
         Note note = new Note();
         note.setId(rs.getInt("id"));
         note.setTitle(rs.getString("title"));
         note.setBody(rs.getString("body"));
-//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm:ss");
         String newdate = rs.getString("data");
         DateFormat formatter = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss"); 
         Date datenew = null;
